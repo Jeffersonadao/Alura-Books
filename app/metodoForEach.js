a9e3e81b498e94b063fe45e2a3f525d893832d50
jefferson Ferreira <jefferson.a.hp@gmail.com>
2023-07-02 02:16:46 -0300

    implantado a opção de verificar livros disponiveis

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -3,9 +3,11 @@ const elementoParaInserirLivros = document.getElementById('livros')
 function exibirOsLivrosNaTela(listaDeLivros) {
     elementoParaInserirLivros.innerHTML = ''  
     listaDeLivros.forEach(livro => {
+      // let disponibilidade = verificarDisponibilidadeDoLivro(livro)  abaixa com o operador ternário
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}"
+        <img class="${disponibilidade}" src="${livro.imagem}"
           alt="${livro.alt}" />
         <h2 class="livro__titulo">
           ${livro.titulo}
@@ -18,4 +20,14 @@ function exibirOsLivrosNaTela(listaDeLivros) {
       </div>
         `
     })
+
+    // if e else da maneira convencional , foi modificado pelo operador ternário
+
+    // function verificarDisponibilidadeDoLivro(livro) {
+    //   if (livro.quantidade > 0) {
+    //     return 'livro__imagens'
+    //   } else {
+    //     return 'livros__imagens indisponivel'
+    //   }
+    // }
 }
